b1e1a4dacd0e1a733f04cc10352f850e2e137752
cloud866 <dklein927@gmail.com>
2023-03-07 22:01:08 +0300

    add thread

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -12,28 +12,15 @@ public class EchoServer {
         ServerSocket server = new ServerSocket(1234);
         while(true) {
             System.out.println("Waiting...");
-
-            // ждем клиента
+            // ждем клиента из сети
             Socket socket = server.accept();
             System.out.println("Client connected!");
-
-            // получаем потоки ввода и вывода
-            InputStream is = ((Socket) socket).getInputStream();
-            OutputStream os = socket.getOutputStream();
-
-            // создаем удобные средства ввода и вывода
-            Scanner in = new Scanner(is);
-            PrintStream out = new PrintStream(os);
-
-            // читаем из сети и пишем в сеть
-            out.println("Welcome to mountains!");
-            String input = in.nextLine();
-            while (!input.equals("bye")){
-                out.println(input + "-" + input + "-" +
-                        input.substring(input.length() / 2) + "...");
-                input = in.nextLine();
-            }
-            socket.close();
+            // создаем клиента на своей стороне
+//            Client client = new Client(socket);
+//            // запускаем поток
+//            Thread thread = new Thread(client);
+//            thread.start();
+            new Thread (new Client(socket)).start();
         }
     }
 }
