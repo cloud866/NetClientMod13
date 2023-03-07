5a8bb83626cfd66241c7ac8c3a761c2bb5d0cb6c
cloud866 <dklein927@gmail.com>
2023-03-07 23:28:35 +0300

    add ChatServer

--- src/Client.java
+++ src/Client.java
@@ -9,9 +9,19 @@ import java.util.Scanner;
 class Client implements Runnable {
     Socket socket;
 
-    public Client(Socket socket){
+    Scanner in;
+    PrintStream out;
+    ChatServer server;
 
+    public Client(Socket socket, ChatServer server){
         this.socket = socket;
+        this.server = server;
+        // запускаем поток
+        new Thread(this).start();
+    }
+
+    void receive(String message) {
+        out.println(message);
     }
 
     public void run() {
@@ -21,15 +31,14 @@ class Client implements Runnable {
             OutputStream os = socket.getOutputStream();
 
             // создаем удобные средства ввода и вывода
-            Scanner in = new Scanner(is);
-            PrintStream out = new PrintStream(os);
+            in = new Scanner(is);
+            out = new PrintStream(os);
 
             // читаем из сети и пишем в сеть
-            out.println("Welcome to mountains!");
+            out.println("Welcome to chat!");
             String input = in.nextLine();
             while (!input.equals("bye")) {
-                out.println(input + "-" + input + "-" +
-                        input.substring(input.length() / 2) + "...");
+                server.sendAll(input);
                 input = in.nextLine();
             }
             socket.close();
